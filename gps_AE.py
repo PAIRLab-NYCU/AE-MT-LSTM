305663feec6f71b3ba8c8b2faa45b4808c4626c6
PAIRLab-NYCU <pairlab.ai.nycu@gmail.com>
2022-07-21 05:23:04 +0800

    gps AE

--- gps_AE.py
+++ gps_AE.py
@@ -10,69 +10,67 @@ import tensorflow as tf
 import tensorflow.keras.backend as K
 
 from tensorflow.keras import layers, losses, regularizers
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import BatchNormalization, Activation, Conv1D, Concatenate, AveragePooling1D, Conv2DTranspose,Lambda
 
 from sklearn import manifold, datasets
+from sklearn.preprocessing import scale
 import matplotlib.pyplot as plt
 
-noise = np.random.normal(scale = 0.05,size=(15))
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# print(tf.__version__)
 
-def create_train_dataset(datas):
+def create_dataset():
+    datas = glob.glob('transfer0508_2/*/*/*.txt')
     train_data = []
-    origin_data = []
-    train_label = []
-    
+    train_label = [] 
     for data in datas:
-        load_data = np.loadtxt(data)#f.read()
-        load_data = load_data[:150,:15].astype(int)
-        load_data[load_data==0] = -100
-        load_data = load_data/100 + 1
-        #print(load_data.shape)
-        for i in range(len(load_data)):
-            origin_data.append(load_data[i])
-            mask = np.random.rand((15))
-            mask = (mask>0.1).astype(int)
-            noise = np.random.normal(scale = 0.05,size=(15))
-            load_data[i]*mask
-            load_data[i]+noise
-            train_data.append(load_data[i])
-            train_label.append(data.split("/")[-1].split("_")[-1].split(".")[0])
+        if data.find('timestamp') == -1:
+            f = np.loadtxt(data,delimiter=" ").copy()
+            f = f/[100,10,100,1,10,100,1e+13,1,1e+3,1e+15]
+            f = f[[0,1,2,4]]
+            train_data.append(f) #/100000000
+            if data.split('/')[1] == "indoor":
+                train_label.append("indoor"+data.split('/')[-2])
+            else:
+                train_label.append("outdoor"+data.split('/')[-2])
+            
 
-    origin_data = np.array(origin_data).astype('float32') 
     train_data = np.array(train_data).astype('float32')
-    train_label = np.array(train_label).astype('int32')
+    train_label = np.array(pd.get_dummies(train_label)).astype('float32')
 
-    train_x = train_data
-    train_o = origin_data
-    train_y = tf.one_hot(train_label,50)
-    BUFFER_SIZE = train_x.shape[0]
-    BATCH_SIZE = int(train_x.shape[0]/2)
+    train_val_split = np.random.rand(len(train_data)) < 0.70
+    train_x = train_data[train_val_split]
+    train_y = train_label[train_val_split]
+    val_x = train_data[~train_val_split]
+    val_y = train_label[~train_val_split]
 
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_x,train_o,train_y))
-    train_dataset = train_dataset.shuffle(BUFFER_SIZE)
-    train_dataset = train_dataset.batch(BATCH_SIZE)
+    return train_x, train_y, val_x, val_y
 
-    return train_dataset
-
-def create_test_dataset(datas):
-    test_data = []
-    test_label = []
+def create_night_dataset():
+    datas = glob.glob('transfer0508_night_2/*/*/*.txt')
+    train_data = []
+    train_label = [] 
     for data in datas:
-        load_data = np.loadtxt(data)#f.read()
-        load_data = load_data[150:200,:15].astype(int)
-        load_data[load_data==0] = -100
-        load_data = load_data/100 + 1
-        for i in range(len(load_data)):
-            test_data.append(load_data[i])
-            test_label.append(data.split("/")[-1].split("_")[-1].split(".")[0])
-    test_data = np.array(test_data).astype('float32')
-    test_label = np.array(test_label).astype('int32')
-    val_x = test_data
-    val_y = tf.one_hot(test_label,50)
+        if data.find('timestamp') == -1:
+            f = np.loadtxt(data,delimiter=" ").copy()
+            f = f/[100,10,100,1,10,100,1e+13,1,1e+3,1e+15]
+            f = f[[0,1,2,4]]
+            train_data.append(f)
+            if data.split('/')[1] == "indoor":
+                train_label.append("indoor"+data.split('/')[-2])
+            else:
+                train_label.append("outdoor"+data.split('/')[-2])
+            
+    train_data = np.array(train_data).astype('float32')
+    train_label = np.array(pd.get_dummies(train_label)).astype('float32')
 
-    return val_x, val_y, test_data, test_label
+    train_val_split = np.random.rand(len(train_data)) < 0.70
 
+    val_x2 = train_data[~train_val_split]
+    val_y2 = train_label[~train_val_split]
+
+    return val_x2, val_y2
 
 def H_l(k, bottleneck_size, kernel_width):
     """ 
@@ -162,58 +160,60 @@ def Conv1DTranspose(filters, kernel_size, strides=2, padding='same'):
         return x
     return f
 
+
 def create_model():
-    inp = tf.keras.layers.Input(shape=(15), name='input_layer1')
-    model = tf.keras.layers.Reshape((15,1))(inp)
+    inp = tf.keras.layers.Input(shape=(4), name='input_layer1')
+    model = tf.keras.layers.Reshape((4,1))(inp)
     model = tf.keras.layers.Conv1D(32,3)(model)
-    model = tf.keras.layers.AveragePooling1D(strides=1)(model)
+    model = tf.keras.layers.AveragePooling1D(strides=1,padding="same")(model)
     model = dense_block(32,3,3,4)(model)
     model = transition_block(stride=1)(model)
+    model = dense_block(4,3,3,4)(model)
     model_down = tf.keras.Model(inputs=[inp], outputs=model)
 
-    inp = tf.keras.layers.Input(shape=(15), name='input_layer2')
+    inp = tf.keras.layers.Input(shape=(4), name='input_layer2')
     model = model_down(inp)
     model = Conv1DTranspose(32,3,1,'valid')(model)
     model = tf.keras.layers.AveragePooling1D(strides=1,padding='same')(model)
     model = Conv1DTranspose(1,3,1,'valid')(model)
-    model = tf.keras.layers.AveragePooling1D(strides=1,padding='same')(model)
-    model = tf.keras.layers.Reshape((15,))(model)
+    model = tf.keras.layers.AveragePooling1D(strides=1)(model)
+    model = tf.keras.layers.Reshape((4,))(model)
     model_encoder_decoder = tf.keras.Model(inputs=[inp], outputs=model)
 
     return model_down, model_encoder_decoder
 
-# @tf.function
-def train_step(t_x,t_o,t_y):
-    with tf.GradientTape() as AE_tape:
+def train_step(t_x,t_y):
+    with tf.GradientTape() as AE_tape,tf.GradientTape() as ANN_tape:
         output = model_encoder_decoder(t_x, training=True)
-        AE_loss = model_loss(output,t_o)
-    gradients_AE = AE_tape.gradient([AE_loss], model_encoder_decoder.trainable_variables)
+        AE_loss = model_loss(output,t_x)
+
+    gradients_AE = AE_tape.gradient(AE_loss, model_encoder_decoder.trainable_variables)
     optimizer_A.apply_gradients(zip(gradients_AE, model_encoder_decoder.trainable_variables))
+    
+    return np.array(AE_loss).mean()#,np.array(ANN_loss).mean()
 
-    return np.array(AE_loss).mean()
-
-def validation(v_x,v_o,v_y):
+def validation(v_x,v_y):
     output = model_encoder_decoder(v_x)
-    AE_loss = model_loss(v_o, output)
+    mse = losses.MeanSquaredError()
+    AE_loss = mse(v_x,output)
     print("AE loss : {}".format(np.array(AE_loss).mean()))
-
     return np.array(AE_loss).mean()
-
+    
 def train(epochs):
     minimum_delta = 0.00001
     history = {}
     history['val_loss'] = np.zeros(epochs)
-
     for epoch in range(epochs):
         start = time.time()
         all_AE = []
-        for x,o,y in train_dataset:
-            AE_loss = train_step(x,o,y)
+        for x,y in train_dataset:
+            AE_loss = train_step(x,y)
             all_AE.append(AE_loss)
         print("train AE loss : {}".format(np.array(all_AE).mean()))
-        loss = validation(val_x,val_x,val_y)
+        loss = validation(val_x,val_y)
         history['val_loss'][epoch] = loss
         checkpoint.save(file_prefix=checkpoint_prefix)
+
         if epoch > 200: #early stop
                 differences = np.abs(np.diff(history['val_loss'][epoch - 3:epoch], n = 1))
                 check =  differences > minimum_delta        
@@ -221,40 +221,54 @@ def train(epochs):
                     print(differences)
                     print("\n\nEarlyStopping Evoked! Stopping training\n\n")
                     break
+                    
         print(f'Time for epoch {epoch + 1} is {time.time() - start:.4f} sec')
 
 def plot_result():
-    val_latent = model_down(val_x)
-    val_latent = np.array(val_latent).reshape(2500,-1)
-    test = np.random.rand(len(val_latent)) < 1
-    X_tsne = manifold.TSNE(n_components=2, init='pca', n_iter=5000, method='exact').fit_transform(val_latent[test])
-    y = test_label[test].reshape([-1,1])
+    val_latent = model_down(val_x3)
+    val_latent = np.array(val_latent).reshape(val_latent.shape[0],-1)
+    train_val_split = np.random.rand(len(val_latent)) < 0.5
+    X_tsne = manifold.TSNE(n_components=2, init='pca', n_iter=500, method='exact').fit_transform(val_latent[train_val_split])
+    y = val_y3[train_val_split].argmax(axis=1).reshape([-1,1])#.argmax(axis=1)
     x_min, x_max = X_tsne.min(0), X_tsne.max(0)
     X_norm = (X_tsne - x_min) / (x_max - x_min)
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(20, 20))
     cm = plt.cm.get_cmap('CMRmap')
-    for i in range(X_norm.shape[0]):
+
+    for i in range(X_norm.shape[0]):    
         plt.text(X_norm[i, 0], X_norm[i, 1], str(y[i,0]), color=cm(y[i,0]*5), 
                 fontdict={'weight': 'bold', 'size': 9})
     plt.xticks([])
     plt.yticks([])
+    #plt.savefig("result/gps_pca_latent16_0710_LPS_2")
     plt.show()
 
 if __name__ == "__main__":
-    datas = glob.glob('0508_UIA/wifi_mag/CIPS-DataCollect/dataRssi_at_*.txt')
-    train_dataset = create_train_dataset(datas)
-    val_x, val_y, test_data, test_label = create_test_dataset(datas)
+    train_x, train_y, val_x, val_y = create_dataset()
+    val_x2, val_y2 = create_night_dataset()
+
+    val_x3 = np.concatenate((val_x,val_x2))
+    val_y3 = np.concatenate((val_y,val_y2))
+
+    BUFFER_SIZE = train_x.shape[0]
+    BATCH_SIZE = train_x.shape[0]
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_x,train_y))
+    train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+    valid_dataset = tf.data.Dataset.from_tensor_slices((val_x3,val_y3)).batch(len(val_x3))
 
     model_down, model_encoder_decoder = create_model()
     # model_down.summary()
-
+    # model_encoder_decoder.summary()
+    
     model_loss = losses.MeanSquaredError()
-    optimizer_A = tf.optimizers.Adam()
+    learning_rate_A = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-2, decay_steps=100, decay_rate=0.9)
+    optimizer_A = tf.optimizers.Adam() #SGD(learning_rate=learning_rate_A , momentum=1e-5)
+    learning_rate_B = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=1e-2, decay_steps=100, decay_rate=0.9)
+    optimizer_B = tf.optimizers.Adam(learning_rate=learning_rate_B)
 
-    checkpoint_dir = './wifi_checkpoints/checkpoints_0524_conv'
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_dropout_model_{epoch}")
+    checkpoint_dir = './gps_checkpoints/checkpoints_0710_LPS_2_2'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_0710_model_{epoch}")
     checkpoint = tf.train.Checkpoint(optimizerA=optimizer_A, model_encoder_decoder=model_encoder_decoder, model_down=model_down)
 
-    train(2000)
-    
+    train(20000)
     plot_result()
